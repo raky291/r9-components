4f5976c0ee8aa740bf58f91a15022247221333eb
raky291 <raky291@gmail.com>
2020-02-10 14:56:08 -0700

    we need to rewrite everything

--- js/src/navbar.js
+++ js/src/navbar.js
@@ -1,11 +1,35 @@
 import $ from 'jquery';
+import Plugin from './plugin';
+
+const NAME = 'navbar';
+const DATA_KEY = 'r9.navbar';
+// const VERSION = '4.4.1';
+// const EVENT_KEY = `.${DATA_KEY}`;
+// const DATA_API_KEY = '.data-api';
+// const JQUERY_NO_CONFLICT = $.fn[NAME];
+
+const Default = {};
+
+const Event = {
+    LOAD_DATA_API: 'load'
+};
+
+const Selector = {
+    DATA_SPY: '[data-spy="scroll"]'
+};
 
 class Navbar {
-    // constructor(element, options) {}
+    constructor(element, options) {}
+
+    static get Default() {
+        return Default;
+    }
 }
 
-$(window).on('load', () => {
-    $('[data-toggle="navbar"]').each(() => {
+Plugin(NAME, DATA_KEY, Navbar);
+
+$(window).on(Event.LOAD_DATA_API, () => {
+    $(Selector.DATA_SPY).each(() => {
         // var $this = $(this);
     });
 });
