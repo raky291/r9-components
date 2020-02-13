0a5c2972c0ab6154544753ebc466e9094da819e2
raky291 <raky291@gmail.com>
2020-02-13 16:47:48 -0700

    update plugin.js

--- js/src/plugin.js
+++ js/src/plugin.js
@@ -1,14 +1,38 @@
 import $ from 'jquery';
 
 class Plugin {
-    static instance() {}
+    constructor(name, dataKey) {
+        this.name = name;
+        this.dataKey = dataKey;
+    }
 
-    static init() {
-        const JQUERY_NO_CONFLICT = $.fn[this.NAME];
-        $.fn[this.NAME] = this.instance;
-        $.fn[this.NAME].Constructor = this;
-        $.fn[this.NAME].noConflict = () => {
-            $.fn[this.NAME] = JQUERY_NO_CONFLICT;
+    instance(option) {
+        // eslint-disable-next-line func-names
+        return this.each(function() {
+            let data = $(this).data(this.dataKey);
+            const options = typeof option === 'object' && option;
+
+            if (!data) {
+                data = new ScrollSpy(this, options);
+                $(this).data(this.dataKey, data);
+            }
+
+            if (typeof option === 'string') {
+                if (typeof data[option] === 'undefined') {
+                    throw new TypeError(`No method named "${option}"`);
+                }
+
+                data[option]();
+            }
+        });
+    }
+
+    init() {
+        const JQUERY_NO_CONFLICT = $.fn[this.name];
+        $.fn[this.name] = this.instance;
+        $.fn[this.name].Constructor = this;
+        $.fn[this.name].noConflict = () => {
+            $.fn[this.name] = JQUERY_NO_CONFLICT;
             return this.instance;
         };
     }
